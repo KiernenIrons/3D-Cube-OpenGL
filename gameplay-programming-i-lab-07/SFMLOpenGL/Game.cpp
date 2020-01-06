c7674dc3d9b0ca90a41e172c6ebda146132d21ef
KiernenIrons <c00241492@itcarlow.ie>
2020-01-06 10:44:29 +0000

    First Commit

---
+++ gameplay-programming-i-lab-07/SFMLOpenGL/Game.cpp
@@ -0,0 +1,127 @@
+#include <Game.h>
+
+bool updatable = false;
+
+Game::Game() : window(VideoMode(800, 600), "OpenGL Cube")
+{
+	index = glGenLists(1);
+}
+
+Game::~Game(){}
+
+void Game::run()
+{
+	initialize();
+
+	Event event;
+
+	while (isRunning){
+
+		cout << "Game running..." << endl;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+			{
+				isRunning = false;
+			}
+		}
+		update();
+		draw();
+	}
+}
+
+void Game::initialize()
+{
+	isRunning = true;
+
+	glClearColor(0.0f, 0.2f, 0.0f, 0.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0);
+	glMatrixMode(GL_MODELVIEW);
+
+	static const GLfloat triangle_Data[] = {
+	-1.0f,-1.0f,-1.0f, // triangle 1 : begin
+	-1.0f,-1.0f, 1.0f,
+	-1.0f, 1.0f, 1.0f, // triangle 1 : end
+	1.0f, 1.0f,-1.0f, // triangle 2 : begin
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f,-1.0f, // triangle 2 : end
+	1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	-1.0f,-1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f,-1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f
+	};
+
+}
+
+void Game::update()
+{
+	elapsed = clock.getElapsedTime();
+
+	if (elapsed.asSeconds() >= 1.0f)
+	{
+		clock.restart();
+
+		if (!updatable)
+		{
+			updatable = true;
+		}
+		else
+			updatable = false;
+	}
+
+	if (updatable)
+	{
+		rotationAngle += 0.005f;
+
+		if (rotationAngle > 360.0f)
+		{
+			rotationAngle -= 360.0f;
+		}
+	}
+	
+	cout << "Update up" << endl;
+}
+
+void Game::draw()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
+
+	window.display();
+}
+
+void Game::unload()
+{
+	cout << "Cleaning up" << endl;
+}
+
---
+++ gameplay-programming-i-lab-07/SFMLOpenGL/Vector3.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <math.h>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class Vector3
+{
+public:
+
+	// Constructor 1
+	Vector3();
+
+	// Constructor 2
+	Vector3(double x1, double y1, double z1);
+
+	double Length();
+	double LengthSquared();
+	void Normalise();
+
+	Vector3 operator+(const Vector3 t_right);
+
+	Vector3 operator-(Vector3 V1);
+
+	Vector3 operator-();
+
+	double operator*(Vector3 V1);
+
+	Vector3 operator*(double k);
+
+	Vector3 operator*(float k);
+
+	Vector3 operator*(int k);
+
+	Vector3 operator^(Vector3 V1);
+
+	std::string ToString();
+
+	double m_x;
+	double m_y;
+	double m_z;
+
+
+
+private:
+
+
+};
